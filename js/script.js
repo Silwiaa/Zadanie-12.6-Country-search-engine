6d0c7816f036a1991b872147b84fc6bd77057446
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-24 20:10:25 +0200

    ready to tests

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 $(function () {
     // URL VARIABLE
    var url = 'https://restcountries.eu/rest/v1/name/',
-       countriesList = $('#countries'); 
+       countriesList = $('#countries'),
+       capitalsList = $('#capitals');
     
     // SEARCH BUTTON
     $('#search').click(searchCountries);
@@ -10,21 +11,23 @@ $(function () {
     function searchCountries() {
         var countryName = $('#country-name').val();
         
-        if(!countryName.lenght)countryName='Poland';
+        if(!countryName.lenght) countryName='Poland';
     
         $ajax({
-            url:url + countryName,
+            url: url + countryName,
             method: 'GET',
-            SUCCESS: showCountriesList
+            success: showCountriesList
         });
     }
     
     // SHOWING RESULTS 
     function showCountriesList(resp) {
         countriesList.empty();
-        resp.firEach(function(item) {
+        capitalsList.empty();
+        
+        resp.forEach(function(item) {
             $('<li>').text(item.name).appendTo(countriesList);
-            
+            $('li').text(item.capital).appendTo(capitalsList);
         });
     }
 });
